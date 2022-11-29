aff1d14f3f4f35ce9eec93d7f7003208e3248589
vikram7102 <bhosalevikram7102@gmail.com>
2022-11-29 11:55:43 +0530

    done

---
+++ Spring_IOC/src/main/java/org/tnsindia/springioc/Airtel.java
@@ -0,0 +1,15 @@
+package org.tnsindia.springioc;
+
+public class Airtel implements Sim{
+
+	public void calling() {
+		System.out.println("calling using airtel");
+		
+	}
+
+	public void data() {
+		System.out.println("internet using airtel");
+		
+	}
+
+}
